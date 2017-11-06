e4b08d227a7751db7fdec6175b9aa3d2d917a42f
Matt Robinson <git@nerdoftheherd.com>
2017-11-06 21:15:40 +0000

    Validate given frame length against actual
    Switch to memcpy-ing the frame header and trailer into packed bitfields
    to make extracting the values simpler and require less magic numbers by
    using sizeof() the structs.
    
    Extract the length field from the frame header and validate this against
    the actual length of the received data.

--- Frame.cpp
+++ Frame.cpp
@@ -1,32 +1,56 @@
 // Copyright © 2017 Matt Robinson
 
+#include <cstring>
 #include <stdexcept>
 
 #include "Frame.h"
 
-static const int MIN_FRAME_SIZE = 3;
-
 static const uint8_t START_BYTE = 0x02;
 static const uint8_t END_BYTE   = 0x03;
 
+static const int length_adjust = 2;
+
+struct Header
+{
+    uint8_t start : 8;
+    uint16_t : 16;
+    uint8_t length : 8;
+} __attribute__((__packed__));
+
+struct Trailer
+{
+    uint8_t end : 8;
+} __attribute__((__packed__));
+
 Frame::Frame(const std::vector<uint8_t> &raw)
 {
-    if(raw.size() < MIN_FRAME_SIZE)
+    if(raw.size() <= sizeof(Header) + sizeof(Trailer))
     {
         throw std::runtime_error("Not enough data for a valid frame");
     }
 
-    if(raw[0] != START_BYTE)
+    struct Header header;
+    memcpy(&header, &raw[0], sizeof(header));
+
+    if(header.start != START_BYTE)
     {
         throw std::runtime_error("Incorrect start byte value");
     }
 
-    if(raw.back() != END_BYTE)
+    if(header.length + length_adjust != raw.size())
+    {
+        throw std::runtime_error("Header length does not match actual length");
+    }
+
+    struct Trailer trailer;
+    memcpy(&trailer, &raw[raw.size() - sizeof(trailer)], sizeof(trailer));
+
+    if(trailer.end != END_BYTE)
     {
         throw std::runtime_error("Incorrect end byte value");
     }
 
-    data = std::vector<uint8_t>(raw.begin() + 1, raw.begin() + (raw.size() - 1));
+    data = std::vector<uint8_t>(raw.begin() + sizeof(header), raw.begin() + (raw.size() - sizeof(trailer)));
 }
 
 std::vector<uint8_t> Frame::getData()
--- tests.cpp
+++ tests.cpp
@@ -19,15 +19,20 @@ BOOST_AUTO_TEST_CASE(frame_no_data)
 
 BOOST_AUTO_TEST_CASE(frame_wrong_start)
 {
-    BOOST_CHECK_THROW(create_frame({ 0x0, 0x0, 0x3 }), std::runtime_error);
+    BOOST_CHECK_THROW(create_frame({ 0x0, 0x0, 0x0, 0x4, 0x0, 0x3 }), std::runtime_error);
 }
 
 BOOST_AUTO_TEST_CASE(frame_wrong_end)
 {
-    BOOST_CHECK_THROW(create_frame({ 0x2, 0x0, 0x0 }), std::runtime_error);
+    BOOST_CHECK_THROW(create_frame({ 0x2, 0x0, 0x0, 0x4, 0x0, 0x0 }), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(frame_wrong_length)
+{
+    BOOST_CHECK_THROW(create_frame({ 0x2, 0x0, 0x0, 0xF, 0x0, 0x0 }), std::runtime_error);
 }
 
 BOOST_AUTO_TEST_CASE(frame_valid)
 {
-    BOOST_CHECK_NO_THROW(create_frame({ 0x2, 0x0, 0x3 }));
+    BOOST_CHECK_NO_THROW(create_frame({ 0x2, 0x0, 0x0, 0x4, 0x0, 0x3 }));
 }
