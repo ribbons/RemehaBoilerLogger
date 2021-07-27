3fb38666f2dcdcb4cedfd4934e39ad89fb4d482c
Matt Robinson <git@nerdoftheherd.com>
2021-07-27 20:56:40 +0100

    Use overcommit to check for issues on commit
    Enable the built in TrailingWhitespace and YamlLint hooks as well as
    adding four custom hooks to check file encoding, newlines at the end of
    files & copyright headers and to run perlcritic.
    
    Also add an extra job to the CI workflow to run all the hooks.

---
+++ .git-hooks/pre_commit/perl_critic.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Copyright © 2021 Matt Robinson
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+module Overcommit
+  module Hook
+    module PreCommit
+      class PerlCritic < Base
+        MESSAGE_REGEX = /^(?<file>.+):(?<line>\d+):\d:/.freeze
+
+        def run
+          result = execute(command, args: applicable_files)
+          return :pass if result.success?
+
+          # example message:
+          #   path/to/file.pl:89:1:Useless '## no critic' annotation
+          extract_messages(
+            result.stdout.chomp.split("\n"),
+            MESSAGE_REGEX
+          )
+        end
+      end
+    end
+  end
+end
